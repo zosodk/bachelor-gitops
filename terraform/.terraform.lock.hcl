3833698a628dd1bc5de515f8a8d44ae05c8a260f
Klaus Hviid <klahvi01@easv365.dk>
2025-12-04 16:09:12 +0000

    fixet proxmox3 provider

---
+++ terraform/.terraform.lock.hcl
@@ -0,0 +1,24 @@
+# This file is maintained automatically by "tofu init".
+# Manual edits may be lost in future updates.
+
+provider "registry.opentofu.org/telmate/proxmox" {
+  version     = "3.0.2-rc06"
+  constraints = "3.0.2-rc06"
+  hashes = [
+    "h1:8pndIIFvNP5t1TmH61O1e3AneqMGFtPYCtp3dQWn5kg=",
+    "zh:004c7913e3ebbe92b07bea13ff340c4f1bc9526bb12abab18aee484a3e512e28",
+    "zh:097695a5e581d89dbc04a99bc4a3e351a57fdb9c0ca44d06f8fd4279679cbaa7",
+    "zh:286633f2bd14b4cfb19335603a9c936139dafd5918918479a1f924e778bfae61",
+    "zh:345bc6c92e79d67c0b8b6db6fab6b361e5e9c3a83c00ca294eb5ba97ed510feb",
+    "zh:452b2b9d6c2b705a82f9ca96abad9916dd94f49e10525a80d5f5004266e44e55",
+    "zh:480d100ba3b40cb2a9e415040f56a83e9ef6590414948ac0e7cc5df9e7520d8e",
+    "zh:649bb8a40f6480b782dc37b81e17774fa9f21a8447e94feed9e414ff48d8b999",
+    "zh:88d23599a199a6178399713f7fdcc602feb1b5189ed2e8121cc9a2e9d9da0828",
+    "zh:99aa3dca58ef5674b737307552c10e931b4d1f6fe618c90a408dcd9457bb3720",
+    "zh:9d9864c8434ffcfef706fd2bbfaf859e8a63d2886320467ded9a26ed3997d225",
+    "zh:ac85549991ee3c5efaedf049ebdfe8a31beaddb71ebeef369d22d9ce61f98165",
+    "zh:bcfba119cb308a668c47f9b2cc9acc3a41e01ec1483bfe3ac9e2ef31922efdad",
+    "zh:e100ad34e1054747078ac1e012b62b42ed7148cc0cbb64c1f22d548e85ddc15b",
+    "zh:f64518ad982294f4b7043ba5ebda47d6b952e8798308ce82899c79200abbe9cb",
+  ]
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -5,22 +5,21 @@ terraform {
   required_providers {
     proxmox = {
       source  = "telmate/proxmox"
-      version = "~> 2.9"
+      version = "3.0.2-rc06"
     }
   }
 }
 
 provider "proxmox" {
   pm_api_url  = "https://192.168.8.20:8006/api2/json" # API endpoint (f.eks. PVE2)
-  pm_user     = var.proxmox_api_user
-  pm_token_id = var.proxmox_token_id
-  pm_token    = var.proxmox_token
-  pm_tls_insecure = true
+#  pm_user     = var.proxmox_token_id
+#  pm_password = var.proxmox_token
+  pm_tls_insecure        = true
+  pm_minimum_permission_check = false
 }
 
 # --- Oprettelse af K8s VM'er ---
 resource "proxmox_vm_qemu" "k8s_node" {
-  # Bruger 'for_each' til at iterere gennem de noder, der er defineret i variables.tf
   for_each    = var.k8s_nodes
 
   target_node = each.value.pve_host # Fordeler noder mellem pve2 og pve3
@@ -39,19 +38,23 @@ resource "proxmox_vm_qemu" "k8s_node" {
     storage = each.value.storage_name # Vælger den hurtige, lokale storage
     size    = "40G"
     type    = "scsi"
-    # Sørg for at boote fra denne disk (rettes automatisk af Tofu/Provider)
+    slot    = "0"
   }
 
   # NETVÆRK 1 (net0): Offentligt/Management (192.168.8.x)
   network {
     bridge  = "vmbr0" 
+    model = "virtio"
+    id   = "0"
   }
   
   # NETVÆRK 2 (net1): Privat/Cluster Replikering (10.0.0.x)
   # Denne VM vil have en ekstra interface, der skal konfigureres af Ansible senere
   network {
     bridge  = "vmbr1" # Skal matche netværksbridge
-  }
+    model   = "virtio"
+    id      = "0"
+}
   
   # Cloud-Init (Sætter kun 192-nettet, da 10-nettet opsættes i Ansible)
   ipconfig0 = "ip=192.168.8.${each.value.ip}/24,gw=192.168.8.1"
