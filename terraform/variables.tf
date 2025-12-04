ae6c60f15f08cef6975e827033884828a3980eec
Klaus Hviid <klahvi01@easv365.dk>
2025-12-04 17:29:45 +0100

    variabler på flere linier

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,37 @@
 # /terraform/variables.tf
 
-variable "proxmox_api_url" { type = string }
-variable "proxmox_api_user" { type = string }
-variable "proxmox_token_id" { type = string }
-variable "proxmox_token_secret" { type = string; sensitive = true }
-variable "proxmox_password" { type = string; sensitive = true }
-variable "proxmox_template_name" { type = string }
+# --- Autentificering (Matcher credentials.auto.tfvars) ---
+variable "proxmox_api_url" {
+  type = string
+}
 
-# --- SIMPEL TEST NODE ---
-variable "test_vmid" { default = 30199 }
-variable "test_ip"   { default = 199 }
+variable "proxmox_api_user" {
+  type = string
+}
+
+variable "proxmox_token_id" {
+  type = string
+}
+
+variable "proxmox_token_secret" {
+  type = string
+  sensitive = true
+}
+
+variable "proxmox_password" {
+  type = string
+  sensitive = true
+}
+
+variable "proxmox_template_name" {
+  type = string
+}
+
+# --- SIMPEL TEST NODE VARIABLER ---
+variable "test_vmid" {
+  default = 30199
+}
+
+variable "test_ip" {
+  default = 99
+}
